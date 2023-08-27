713c623720c6cadb416de6afba26944e04aae45e
Katia Ghezali <ghezalikatia@hotmail.com>
2023-08-27 18:05:43 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+
+const dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+    const left = parseInt(dodger.style.left);
+    
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+  
+  function moveDodgerRight() {
+    const left = parseInt(dodger.style.left);
+    
+    if (left < (400 - 40)) { 
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
+  
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+  //PS: I am still trying top wrap my head around the code, a loot of it was copy pasted :D
